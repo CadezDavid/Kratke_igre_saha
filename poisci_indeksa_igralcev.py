4ec106d4824d1945a253306e821c5bd091d04c0e
CadezDavid <david.cadez7@gmail.com>
2020-11-05 15:41:47 +0100

    Prebere ze nalozene html-je

--- poisci_indeksa_igralcev.py
+++ poisci_indeksa_igralcev.py
@@ -2,12 +2,7 @@ import re
 import orodja
 
 
-
-
 def poisci_indeksa_igralcev(indeks):
-    
-    with open(f'datoteke_igre/{indeks}.html') as f:
-        vsebina = f.read()
 
     vzorec = (
         r'<b><a href="/perl/chessplayer\?pid=(?P<prvi_id>\d{5,7})">.+</a></b>\n'
@@ -15,5 +10,10 @@ def poisci_indeksa_igralcev(indeks):
         r'<b><a href="/perl/chessplayer\?pid=(?P<drugi_id>\d{5,7})">.+</a></b>'
     )
 
+    with open(f'datoteke_igre/{indeks}.html') as f:
+        vsebina = f.read()
+
     for zadetek in re.finditer(vzorec, vsebina):
         return zadetek.groupdict()
+
+    return {'prvi_id': None, 'drugi_id': None}
--- poisci_podatke_igralca.py
+++ poisci_podatke_igralca.py
@@ -3,15 +3,20 @@ import orodja
 
 def poisci_podatke_igralca(indeks):
 
+    if indeks == None:
+        print('Nisem dobil smiselnega indeks ... :(')
+        return {'letnica_rojstva': None, 'drzava_rojstva': None} 
+
     vzorec = (
-        r'<font size=-2>\(born .*(?P<letnica_rojstva>\d{4}).*\) (?P<drzava_rojstva>.*)</font>'
+        r'<font size=-2>\(.*(?P<letnica_rojstva>\d{4}).*\)(?P<drzava_rojstva>.*)</font>'
         r'</font></td>\n'
         r'<td background="/chessimages/chessgamesbio\.gif" width=212 align=RIGHT>'
     )
 
-    celoten_html = orodja.nalozi_stran(f'https://www.chessgames.com/perl/chessplayer?pid={indeks}')
+    with open(f'datoteke_igralci/{indeks}.html') as f:
+        vsebina = f.read()
 
-    for zadetek in re.finditer(vzorec, celoten_html):
+    for zadetek in re.finditer(vzorec, vsebina):
         return zadetek.groupdict()
 
     return {'letnica_rojstva': None, 'drzava_rojstva': None}
