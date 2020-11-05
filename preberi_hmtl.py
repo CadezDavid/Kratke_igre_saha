433aefec37e245e28e96a24d92d22985ad6443d6
CadezDavid <david.cadez7@gmail.com>
2020-11-05 15:41:15 +0100

    Zdaj izdela tudi json s podatki o igralcih

--- preberi_hmtl.py
+++ preberi_hmtl.py
@@ -36,6 +36,8 @@ vzorec = (
 )
 
 igre = list()
+igralci = list()
+stevec = 0
 
 for stran in range(200):
     with open(f'datoteke/kratke_igra_saha_stran_{stran}.html') as f:
@@ -44,19 +46,32 @@ for stran in range(200):
         for zadetek in re.finditer(vzorec, vsebina):
             podatki_igre = zadetek.groupdict()
 
-            shrani_html_igre(podatki_igre['indeks_igre'])
+            idja_igralcev = poisci_indeksa_igralcev.poisci_indeksa_igralcev(podatki_igre['indeks_igre'])
+            if idja_igralcev['prvi_id']:
+                podatki_prvega = poisci_podatke_igralca.poisci_podatke_igralca(idja_igralcev['prvi_id'])
+                podatki_drugega = poisci_podatke_igralca.poisci_podatke_igralca(idja_igralcev['drugi_id'])
 
-            shrani_html_igralcev(podatki_igre['indeks_igre'])
+            podatki_igre['letnica_prvega'] = podatki_prvega['letnica_rojstva']
+            podatki_igre['letnica_drugega'] = podatki_drugega['letnica_rojstva']
+            podatki_igre['drzava_prvega'] = podatki_prvega['drzava_rojstva']
+            podatki_igre['drzava_drugega'] = podatki_drugega['drzava_rojstva']
 
-            # idja_igralcev = poisci_indeksa_igralcev.poisci_indeksa_igralcev(podatki_igre['indeks_igre'])
-            # podatki_prvega = poisci_podatke_igralca.poisci_podatke_igralca(idja_igralcev['prvi_id'])
-            # podatki_drugega = poisci_podatke_igralca.poisci_podatke_igralca(idja_igralcev['drugi_id'])
+            if podatki_igre['letnica_prvega'] and podatki_igre['letnica_drugega'] and podatki_igre['drzava_prvega'] and podatki_igre['drzava_drugega']:
+                stevec += 1
+
             for podatek in podatki_igre:
-                podatki_igre[podatek] = podatki_igre[podatek].strip()
-            # podatki_igre['letnica_prvega'] = podatki_prvega['letnica_rojstva']
-            # podatki_igre['letnica_drugega'] = podatki_drugega['letnica_rojstva']
-            # podatki_igre['drzava_prvega'] = podatki_prvega['drzava_rojstva']
-            # podatki_igre['drzava_drugega'] = podatki_drugega['drzava_rojstva']
+                if podatki_igre[podatek] != None:
+                    podatki_igre[podatek] = podatki_igre[podatek].strip()
+
+            igralci.append(
+                {'id': idja_igralcev['prvi_id'],'ime': podatki_igre['prvi_igralec'], 'letnica_rojstva': podatki_igre['letnica_prvega'], 'drzava_rojstva': podatki_igre['drzava_prvega']}
+            )
+            igralci.append(
+                {'id': idja_igralcev['drugi_id'],'ime': podatki_igre['drugi_igralec'], 'letnica_rojstva': podatki_igre['letnica_drugega'], 'drzava_rojstva': podatki_igre['drzava_drugega']}
+            )
+
             igre.append(podatki_igre)
 
+
+orodja.zapisi_json(igralci, 'izlusceni_igralci.json')
 orodja.zapisi_json(igre, 'izluscene_igre.json')
